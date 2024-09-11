765b89f98e18f50041116553e8b61275ff5356b5
texotek <fdahmen@proton.me>
2024-09-11 17:12:49 +0200

    fixed bug maybe?

--- dn_audiotranscriber/transcriber.py
+++ dn_audiotranscriber/transcriber.py
@@ -1,4 +1,3 @@
-import numpy as np
 import whisper
 
 def transcribe_audio(filepath: str):
@@ -7,10 +6,9 @@ def transcribe_audio(filepath: str):
     audio = whisper.load_audio(filepath)
     audio = whisper.pad_or_trim(audio)
 
-    mel = whisper.log_mel_spectrogram(audio=audio, n_mels=128).to(model.device)
-    _, probs = model.detect_language(mel)
-
-    print(f"Detected language: {max(probs, key=probs.get)}")
+    mel = whisper.log_mel_spectrogram(audio=audio).to(model.device)
+    # _, probs = model.detect_language(mel)
+    # print(f"Detected language: {max(probs, key=probs.get)}")
 
     options = whisper.DecodingOptions()
     result = whisper.decode(model, mel, options)
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from dn_audiotranscriber import transcribe_audio, record_audio
+from dn_audiotranscriber import transcribe_audio 
+from soundrecorder import record_audio, save_audio, delete_audio
 
 
-data = record_audio()
-result = transcribe_audio(data)
-
-print(result["text"])
+audio_data = record_audio()
+save_audio(audio_data, "test.wav")
+try:
+    result = transcribe_audio("test.wav")
+    print(result.text)
+finally:
+    delete_audio("test.wav")
