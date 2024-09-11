343cba113b7831f1be98fee9a61edd36d66e7a68
texotek <fdahmen@proton.me>
2024-09-11 11:11:12 +0200

    changed transcriber to use filepath

--- dn_audiotranscriber/transcriber.py
+++ dn_audiotranscriber/transcriber.py
@@ -1,29 +1,17 @@
 import numpy as np
 import whisper
 
-from .soundrecorder import save_audio, delete_audio
+def transcribe_audio(filepath: str):
+    model = whisper.load_model("base")
 
-filename = "temp.wav"
+    audio = whisper.load_audio(filepath)
+    audio = whisper.pad_or_trim(audio)
 
-def transcribe_audio(audio_data):
+    mel = whisper.log_mel_spectrogram(audio=audio, n_mels=128).to(model.device)
+    _, probs = model.detect_language(mel)
 
-    # Save the recorded audio to a WAV file
-    save_audio(audio_data, filename)
-    try:
-        print("Finished recording")
+    print(f"Detected language: {max(probs, key=probs.get)}")
 
-        model = whisper.load_model("base")
-
-        audio = whisper.load_audio(filename)
-        audio = whisper.pad_or_trim(audio)
-
-        mel = whisper.log_mel_spectrogram(audio=audio, n_mels=128).to(model.device)
-        _, probs = model.detect_language(mel)
-
-        print(f"Detected language: {max(probs, key=probs.get)}")
-
-        options = whisper.DecodingOptions()
-        result = whisper.decode(model, mel, options)
-        return result
-    finally:
-        delete_audio(filename)
+    options = whisper.DecodingOptions()
+    result = whisper.decode(model, mel, options)
+    return result
