a2b43b42178df7b5bc36f960da662795bc65d4f3
texotek <fdahmen@proton.me>
2025-02-06 19:02:41 +0100

    remove version things

--- setup.py
+++ setup.py
@@ -4,10 +4,6 @@ import importlib.util
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
-def read_version(fname="dn_audiotranscriber/__version__.py"):
-    exec(compile(open(fname, encoding="utf-8").read(), fname, "exec"))
-    return locals()["__version__"]
-
 setup(
     name='dn_audiotranscriber',
     version="0.0.11",
